30e2b7b10c02194f99c7fd7bc15343b55b6e9bd8
Grok TuanDo <dodangtuan609@gmail.com>
2025-05-30 02:04:06 +0700

    update

--- src/routes/model.route.js
+++ src/routes/model.route.js
@@ -5,7 +5,8 @@ import upload from '../middlewares/upload.js';
 const router = express.Router();
 
 router.get('/', modelController.getListByUser);
+router.get('/:id', modelController.getOne);
 router.post('/', upload.single('file'), modelController.create);
-router.put('/:id', modelController.update);
+router.patch('/:id', modelController.update);
 router.delete('/:id', modelController.delete);
 export default router;
