eda1cf96b91d94bb32dda4b011eb4a37ee890b32
Grok TuanDo <dodangtuan609@gmail.com>
2025-06-01 14:30:23 +0700

    integrate db

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,26 @@
-import mysql from "mysql2/promise.js";
+import mysql from 'mysql2/promise.js';
+
+const options =
+  process.env.NODE_ENV === 'production'
+    ? {
+        host: 'sql12.freesqldatabase.com',
+        user: 'sql12782293',
+        password: 'p5JBVTLvMq',
+        database: 'sql12782293',
+        port: 3306,
+      }
+    : {
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'ar',
+        port: 3306,
+      };
 
 let connection;
 export const database = {
   connect: () => {
-    connection = mysql.createPool({
-      host: "sql12.freesqldatabase.com",
-      user: "sql12782293",
-      password: "p5JBVTLvMq",
-      database: "sql12782293",
-      port: 3306,
-      waitForConnections: true,
-    });
+    connection = mysql.createPool(options);
   },
   execute: async (sql, params = []) => {
     const [rows] = await connection.execute(sql, params);
